98e66232a004a2aac3cd72a7c1b3d324d891b7b0
Mangalam-007 <68485964+Mangalam-007@users.noreply.github.com>
2021-08-15 17:42:37 +0530

    Update SplitwiseAlgorithm.cpp

--- SplitwiseAlgorithm.cpp
+++ SplitwiseAlgorithm.cpp
@@ -1,35 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-class Graph{
-	unordered_map<string,int> graph;
-public:
-	void addAmount(string user,int amount){
-		if(graph.find(user)==graph.end()){
-			graph[user] = amount;
-		}else{
-			graph[user] = graph[user]+amount;
-		}
+#define ll long long
+class Graph
+{
+	map<string,int> g;
+	public:
+	void transaction(string giver,string receiver, int amount)
+	{
+		if(g.find(giver)==g.end())
+			g[giver]=-1*amount;
+		else
+			g[giver]=g[giver]-amount;
+		if(g.find(receiver)==g.end())
+			g[receiver]=amount;
+		else
+			g[receiver]=g[receiver]+amount;
 	}
-	void display(){
-		for(auto e:graph){
-			cout<<e.first<<" "<<e.second<<endl;
+	void splitMoney()
+	{
+		int d,c,amount;
+		set<pair<int,string> > s;
+		for(auto itr:g)
+		{
+			if(itr.second!=0)
+				s.insert({itr.second,itr.first});
+		}
+		while(s.size()>0)
+		{
+			auto debit=s.begin();
+			auto credit=--s.end();
+			s.erase(debit);
+			s.erase(credit);
+			d=debit->first;
+			c=credit->first;
+			// cout<<d<<" "<<c<<endl;
+			string dp=debit->second;
+			string cp=credit->second;
+			amount=min(abs(d),c);
+			cout<<dp<<" will give "<<amount<<" to "<<cp<<endl;
+			d+=amount;
+			c-=amount;
+			if(d!=0)
+				s.insert({d,dp});
+			if(c!=0)
+				s.insert({c,cp});
 		}
 	}
 };
-
-int main(){
-	
-	int no_of_trans,friends;
-	cin>>no_of_trans>>friends;
+int main()
+{
+	int friends,NoOfTransactions,i;
+	cin>>friends;
 	Graph g;
-	while(no_of_trans--){
-		string x,y;
-		int amount;
-		cin>>x>>y>>amount;
-		g.addAmount(y,amount);
-		amount = -amount;
-		g.addAmount(x,amount);
-	}g.display();
-
+	cin>>NoOfTransactions;
+	string giver,receiver;
+	int amount;
+	while(NoOfTransactions--)
+	{
+		cin>>giver>>receiver>>amount;
+		g.transaction(giver,receiver,amount);
+	}
+	g.splitMoney();
 }
